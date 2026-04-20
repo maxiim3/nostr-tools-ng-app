431e5cd4cba997aecdd2842d0e97e8a23f8fe01d
maxime_tamburrini <mtamburrini@agence-ddm.fr>
2026-04-20 13:59:38 +0200

    feat: project info

---
+++ src/core/config/project-info.ts
@@ -0,0 +1,10 @@
+export const PROJECT_INFO = {
+  name: 'NostrTools',
+  ownerNpub: 'npub1zkse38pvfqlkcmcc7tw6zqecj7sqxe5lgj0u9ldylghmdjfppyqqtsa4du',
+  contact: 'maxiim3@primal.net',
+  zapAddress: 'maxiim3@primal.net',
+  calleNpub: 'npub12rv5lskctqxxs2c8rf2zlzc7xx3qpvzs3w4etgemauy9thegr43sf485vg',
+  calleZap: 'npub12rv5lskctqxxs2c8rf2zlzc7xx3qpvzs3w4etgemauy9thegr43sf485vg@npub.cash',
+  followingSpaceUrl: 'https://following.space',
+  followingSpaceGithub: 'https://github.com/callebtc/following.space'
+} as const;
